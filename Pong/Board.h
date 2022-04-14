f8b91b32f3e008c41c4c75c53c34c5975c1b9ac2
Charlotte Wertz <charlotte.wertz@wsu.edu>
2022-04-14 10:11:59 -0700

    added comments

--- Pong/Board.h
+++ Pong/Board.h
@@ -11,6 +11,7 @@ public:
 	Board();
 private:
 	Tile board[100][100];
+
 	// checks which cells on a board live or die
 	void check_board(Tile board[][100]);
 	// checks the surrounding area of a single cell
