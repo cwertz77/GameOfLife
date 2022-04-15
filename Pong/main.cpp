9fa65352d08a24f6bc333715fbd09c0c4800253c
janellekau <janelle.kau@wsu.edu>
2022-04-15 11:18:23 -0700

    test

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -7,12 +7,9 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
-<<<<<<< HEAD
 	sf::CircleShape shape(100.f);
-=======
 	//sf::CircleShape shape(100.f);
 	Board initial_board();
->>>>>>> e7854b55c75769aafe6e483a815aa754dd0ade8c
 
 	//shape.setFillColor(sf::Color::Green);
 
